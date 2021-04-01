2a2a515c4fb8bbf492598771c193b237e4a4022b
Lennart Hildebrandt <lennart.hildebrandt@wps.de>
2021-04-01 13:04:01 +0200

    lh: Initialer Commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Deck, Slide, Heading } from 'spectacle';
+
+function App() {
+  return (
+    <Deck>
+      <Slide>
+        <Heading>Welcome to Spectacle</Heading>
+      </Slide>
+    </Deck>
+  );
+}
+
+export default App;
