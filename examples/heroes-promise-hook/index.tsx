97f5ec0220c952640052ea5ee9963f330f9f4fd8
Lennart Hildebrandt <hello@lennerd.com>
2021-04-08 15:26:54 +0200

    lh: Server-Request über eigenen Promise-Hook

--- examples/heroes-promise-hook/index.tsx
+++ examples/heroes-promise-hook/index.tsx
@@ -1,16 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { QueryClient, QueryClientProvider } from "react-query";
-import { BrowserRouter as Router } from "react-router-dom";
-import TourOfHeroes from "./src/TourOfHeroes";
+import App from "./App";
 
-const queryClient = new QueryClient();
-
-ReactDOM.render(
-  <Router>
-    <QueryClientProvider client={queryClient}>
-      <TourOfHeroes />
-    </QueryClientProvider>
-  </Router>,
-  document.getElementById("root")
-);
+ReactDOM.render(<App />, document.getElementById("root"));
